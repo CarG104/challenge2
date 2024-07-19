64e3b04ab06ed95450567994ea7ac9709f3b4d91
Carlos A Guette <carlosguette5@icloud.com>
2024-07-19 01:27:31 -0500

    Logica lista

--- app.js
+++ app.js
@@ -1,9 +1,62 @@
-function guardarTexto(){
-//Creacion de variables
-let transform;
-let result;
-//Obtener el elemento
-transform = document.getElementById('textarea')
-//Obtener el valor del elemento seleccionado
-result = transform.value;
+document.addEventListener("DOMContentLoaded", function() {
+    // Obtiene el botón por su clase
+    var botones = document.getElementsByClassName("button");
+
+    // Agrega un evento de clic a cada botón
+    for (var i = 0; i < botones.length; i++) {
+        var botonTexto = botones[i].textContent.trim();
+        if (botonTexto === "Encriptar") {
+            botones[i].addEventListener("click", function(event) {
+                event.preventDefault(); // Evita que el enlace recargue la página
+                eliminarElementos();
+                guardarElemento(true); // true indica que queremos encriptar
+            });
+        } else if (botonTexto === "Desencriptar") {
+            botones[i].addEventListener("click", function(event) {
+                event.preventDefault(); // Evita que el enlace recargue la página
+                eliminarElementos();
+                guardarElemento(false); // false indica que queremos desencriptar
+            });
+        }
+    }
+});
+
+// Función para eliminar todos los elementos dentro del aside
+function eliminarElementos() {
+    var aside = document.getElementById("aside");
+    while (aside.firstChild) {
+        aside.removeChild(aside.firstChild);
+    }
+}
+
+function guardarElemento(encryptar) {
+    var texto = document.querySelector("#textarea").value;
+    var aside = document.querySelector("#aside");
+    var nuevoText = document.createElement("p");
+    nuevoText.classList.add("encrypt");
+
+    if (encryptar) {
+        // Encriptar el texto
+        var cifrado = encriptarTexto(texto);
+        nuevoText.textContent = cifrado;
+    } else {
+        // Desencriptar el texto
+        var textoDesencriptado = desencriptarTexto(texto);
+        nuevoText.textContent = textoDesencriptado;
+    }
+
+    aside.appendChild(nuevoText);
+}
+
+function encriptarTexto(texto) {
+    const key = "clave-secreta";
+    const cifrado = CryptoJS.AES.encrypt(texto, key).toString();
+    return cifrado;
+}
+
+function desencriptarTexto(cifrado) {
+    const key = "clave-secreta";
+    const bytes = CryptoJS.AES.decrypt(cifrado, key);
+    const textoDesencriptado = bytes.toString(CryptoJS.enc.Utf8);
+    return textoDesencriptado;
 }
