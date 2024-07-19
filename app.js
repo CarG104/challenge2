f4c4e3c22a71ef7585ed243fce0b2d1c58adfc8f
Carlos A Guette <carlosguette5@icloud.com>
2024-07-18 23:02:54 -0500

    Finalizacion del destok de la pagina y inicios de las funcionalidades

---
+++ app.js
@@ -0,0 +1,9 @@
+function guardarTexto(){
+//Creacion de variables
+let transform;
+let result;
+//Obtener el elemento
+transform = document.getElementById('textarea')
+//Obtener el valor del elemento seleccionado
+result = transform.value;
+}
