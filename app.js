ca5c205e16f6dfe89a6c152015f95ae18453e640
Carlos A Guette <carlosguette5@icloud.com>
2024-08-18 19:55:13 -0500

    Se ha añadido el metodo de encriptacion tal y como se solicitaba en los requisitos del projecto y se ha quitado el de crypto

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
     // Obtiene el botón por su clase
     var botones = document.getElementsByClassName("button");
+    
+
 
     // Agrega un evento de clic a cada botón
     for (var i = 0; i < botones.length; i++) {
@@ -8,14 +10,20 @@ document.addEventListener("DOMContentLoaded", function() {
         if (botonTexto === "Encriptar") {
             botones[i].addEventListener("click", function(event) {
                 event.preventDefault(); // Evita que el enlace recargue la página
-                eliminarElementos();
-                guardarElemento(true); // true indica que queremos encriptar
+                var texto = document.querySelector("#textarea").value;
+                if (validarTexto(texto)) {
+                    eliminarElementos();
+                    guardarElemento(true);// true indica que queremos encriptar
+                }
             });
         } else if (botonTexto === "Desencriptar") {
             botones[i].addEventListener("click", function(event) {
                 event.preventDefault(); // Evita que el enlace recargue la página
-                eliminarElementos();
-                guardarElemento(false); // false indica que queremos desencriptar
+
+                if (validarTexto(texto)) {
+                    eliminarElementos();
+                    guardarElemento(false);// false indica que queremos desencriptar
+                }
             });
         }
     }
@@ -26,6 +34,26 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 });
+document.querySelector("#textarea").addEventListener("input", function() {
+    var texto = this.value;
+    validarTexto(texto);
+});
+ // Función para validar el texto
+ function validarTexto(texto) {
+    var textoMinusculas = texto.toLowerCase();
+    var textoSinAcentos = texto.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+
+    if (texto !== textoMinusculas || texto !== textoSinAcentos) {
+        alert("Por favor, ingrese solo letras minúsculas sin acentos.");
+        document.querySelector("#btnEncriptar").disabled = true;
+        document.querySelector("#btnDesencriptar").disabled = true;
+        return false;
+    } else {
+        document.querySelector("#btnEncriptar").disabled = false;
+        document.querySelector("#btnDesencriptar").disabled = false;
+        return true;
+    }
+}
 
 // Función para eliminar todos los elementos dentro del aside
 function eliminarElementos() {
@@ -37,6 +65,7 @@ function eliminarElementos() {
 
 function guardarElemento(encryptar) {
     var texto = document.querySelector("#textarea").value;
+    console.log("Textarea valor: "+texto)
     var aside = document.querySelector("#aside");
     var nuevoText = document.createElement("p");
     nuevoText.classList.add("encrypt");
@@ -48,6 +77,7 @@ function guardarElemento(encryptar) {
         // Encriptar el texto
         var cifrado = encriptarTexto(texto);
         nuevoText.textContent = cifrado;
+        console.log("texto cifrado en el condicional de la funcion guardar elemento: "+cifrado);
     } else {
         // Desencriptar el texto
         var textoDesencriptado = desencriptarTexto(texto);
@@ -55,20 +85,39 @@ function guardarElemento(encryptar) {
    
     }
     
+
+    console.log("texto para probar si guardar elemento funciona: "+nuevoText.textContent)
     aside.appendChild(nuevoText);
     aside.appendChild(buttonCopy);
 }
 
 function encriptarTexto(texto) {
-    const key = "clave-secreta";
-    const cifrado = CryptoJS.AES.encrypt(texto, key).toString();
+    let reemplazos = {
+        "o": "ober",
+        "a": "ai",
+        "e": "enter",
+        "i": "imes",
+        "u": "ufat"
+    };
+    const cifrado = texto.replace(/[oaieu]/g, function(matched) {
+        return reemplazos[matched];
+    });
+    console.log("Texto de depuracion de la funcion encriptarTexto: "+cifrado)
     return cifrado;
 }
 
-function desencriptarTexto(cifrado) {
-    const key = "clave-secreta";
-    const bytes = CryptoJS.AES.decrypt(cifrado, key);
-    const textoDesencriptado = bytes.toString(CryptoJS.enc.Utf8);
+function desencriptarTexto(texto) {
+    let reemplazos2 = {
+        "ober": "o",
+        "ai": "a",
+        "enter": "e",
+        "imes": "i",
+        "ufat": "u"
+    };
+    const textoDesencriptado = texto.replace(/ober|ai|imes|enter|ufat/g, function(matched) {
+        return reemplazos2[matched];
+    });
+    console.log("Texto de depuracion de la funcion encriptarTexto: " +textoDesencriptado);
     return textoDesencriptado;
 }
 function copiarAlPortapapeles(texto) {
